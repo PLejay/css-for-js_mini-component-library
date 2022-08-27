7b52baeb2c7f797038f3f5cb3fe2f345572c6e70
Pierre Lejay <pierrelejay1@gmail.com>
2022-08-27 10:16:44 +0100

    implement select and iconinput

--- src/components/IconInput/IconInput.js
+++ src/components/IconInput/IconInput.js
@@ -1,19 +1,73 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
+import { COLORS } from "../../constants";
 
-import Icon from '../Icon';
-import VisuallyHidden from '../VisuallyHidden';
+import Icon from "../Icon";
+import VisuallyHidden from "../VisuallyHidden";
 
-const IconInput = ({
-  label,
-  icon,
-  width = 250,
-  size,
-  placeholder,
-}) => {
-  return 'TODO';
+const STYLES = {
+  small: {
+    "--border-width": "1px",
+    "--font-size": "14px",
+    "--icon-bottom": "3px",
+    "--icon-left": "4px",
+    "--padding-left": "24px",
+  },
+  large: {
+    "--border-width": "2px",
+    "--font-size": "18px",
+    "--icon-bottom": "4px",
+    "--icon-left": "4px",
+    "--padding-left": "32px",
+  },
+};
+
+const IconWrapper = styled.div`
+  position: absolute;
+  bottom: var(--icon-bottom);
+  left: var(--icon-left);
+`;
+
+const Wrapper = styled.div`
+  position: relative;
+  border-bottom-width: var(--border-width);
+  border-bottom-style: solid;
+  display: flex;
+  width: ${({ width }) => width + "px"};
+`;
+
+const StyledInput = styled.input`
+  border: none;
+  width: 100%;
+  font-size: var(--font-size);
+  padding-left: var(--padding-left);
+  outline-offset: 4px;
+  border-radius: 2px;
+  color: ${COLORS.gray700};
+  font-weight: 700;
+  &::placeholder {
+    color: ${COLORS.gray500};
+    font-weight: 500;
+  }
+`;
+
+const IconInput = ({ label, icon, width = 250, size, placeholder }) => {
+  const style = STYLES[size];
+  return (
+    <Wrapper {...{ style, width }}>
+      <IconWrapper style={style}>
+        <Icon id={icon} size={size === "small" ? 14 : 18} />
+      </IconWrapper>
+      <StyledInput
+        aria-label={label}
+        type="text"
+        id="icon-input"
+        placeholder={placeholder}
+        style={style}
+      ></StyledInput>
+    </Wrapper>
+  );
 };
 
 export default IconInput;
--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,17 +1,49 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react";
+import styled from "styled-components";
 
-import { COLORS } from '../../constants';
-import Icon from '../Icon';
-import { getDisplayedValue } from './Select.helpers';
+import { COLORS } from "../../constants";
+import Icon from "../Icon";
+import { getDisplayedValue } from "./Select.helpers";
+
+const StyledWrapper = styled.div`
+  border-radius: 8px;
+  background: ${COLORS.transparentGray15};
+  width: min-content;
+  position: relative;
+  padding: 12px 16px;
+  white-space: nowrap;
+  display: flex;
+  align-items: center;
+  column-gap: 16px;
+  color: ${COLORS.gray700};
+
+  &:hover {
+    color: black;
+  }
+  & > select {
+    background: transparent;
+    color: transparent;
+    border: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    bottom: 0;
+    right: 0;
+    border-radius: 8px;
+  }
+`;
 
 const Select = ({ label, value, onChange, children }) => {
   const displayedValue = getDisplayedValue(value, children);
 
   return (
-    <select value={value} onChange={onChange}>
-      {children}
-    </select>
+    <StyledWrapper>
+      <span>{displayedValue}</span>
+      <Icon id="chevron-down" strokeWidth={2} />
+      <select value={value} onChange={onChange}>
+        {children}
+      </select>
+    </StyledWrapper>
   );
 };
 
